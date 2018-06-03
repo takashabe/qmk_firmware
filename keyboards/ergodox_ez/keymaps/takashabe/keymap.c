972c9e42059b8b96b9beb1af9f5ef82dd723abe0
takashabe <takashabe.io@gmail.com>
2018-06-04 00:42:23 +0900

    Base 0.1

--- keyboards/ergodox_ez/keymaps/takashabe/keymap.c
+++ keyboards/ergodox_ez/keymaps/takashabe/keymap.c
@@ -22,41 +22,41 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  [   |           |  ]   |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|' / Cmd |
- * |--------+------+------+------+------+------| Del  |           | Meh  |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
+ * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
+ * |--------+------+------+------+------+------|  =   |           |  -   |------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  L1  |  '"  |      | LAlt |  Cmd |                                       |  Up  | Down |   [  |   ]  | ~L1  |
+ *   |  L1  |  '"  | LAlt | LCmd |  Esc |                                       |  Esc | RCmd | RAlt |      |  L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
- *                                        |      | L1   |       | Alt  |Ctrl/Esc|
+ *                                        |      | L1   |       | L1   |Ctrl/Esc|
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | L2   |       | PgUp |        |      |
- *                                 |Space |Backsp|------|       |------|  Tab   |Enter |
- *                                 |      |ace   | Esc  |       | PgDn |        |      |
+ *                                 |      |      | L2   |       | L2   |        |      |
+ *                                 |Space |Backsp|------|       |------|  Del   |Enter |
+ *                                 |      |ace   | Esc  |       |      |        |      |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_EQL,   KC_1,        KC_2,  KC_3,    KC_4,    KC_5, KC_NO,
-        KC_TAB,   KC_Q,        KC_W,  KC_E,    KC_R,    KC_T, KC_LBRC,
-        KC_LCTRL, KC_A,        KC_S,  KC_D,    KC_F,    KC_G,
-        KC_LSFT,  CTL_T(KC_Z), KC_X,  KC_C,    KC_V,    KC_B, KC_DEL,
-        TG(SYMB), KC_QUOT,     KC_NO, KC_LALT, KC_LGUI,
+        KC_EQL,   KC_1,    KC_2,    KC_3,    KC_4,   KC_5, KC_NO,
+        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,   KC_T, KC_LBRC,
+        KC_LCTRL, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,
+        KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,   KC_B, KC_EQL,
+        TG(SYMB), KC_QUOT, KC_LALT, KC_LGUI, KC_ESC,
                                                       KC_NO,   TG(SYMB),
                                                                TG(MDIA),
                                               KC_SPC, KC_BSPC, KC_ESC,
         // right hand
-        KC_RGHT,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_MINS,
-        KC_RBRC,    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLS,
-                     KC_H,   KC_J,  KC_K,   KC_L,   LT(MDIA, KC_SCLN),GUI_T(KC_QUOT),
-        MEH_T(KC_NO),KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                             KC_UP, KC_DOWN,KC_LBRC,KC_RBRC,          KC_FN1,
-        KC_LALT,        CTL_T(KC_ESC),
-        KC_PGUP,
-        KC_PGDN,KC_TAB, KC_ENT
+        KC_NO,    KC_6, KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+        KC_RBRC,  KC_Y, KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+                  KC_H, KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_MINS,  KC_N, KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                        KC_ESC,  KC_RGUI, KC_RALT, KC_NO,   TG(SYMB),
+        TG(SYMB), KC_ESC,
+        TG(MDIA),
+        KC_NO,    KC_DEL, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
