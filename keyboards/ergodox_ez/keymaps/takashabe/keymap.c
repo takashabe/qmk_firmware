ecc39d9941f116e1d10ff042461fa4b9d2dfdc90
takashabe <abe_takashi@cyberagent.co.jp>
2018-06-25 12:42:32 +0900

    update

--- keyboards/ergodox_ez/keymaps/takashabe/keymap.c
+++ keyboards/ergodox_ez/keymaps/takashabe/keymap.c
@@ -22,17 +22,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  [   |           |  ]   |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
- * |--------+------+------+------+------+------|  =   |           |  -   |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
+ * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  | Enter  |
+ * |--------+------+------+------+------+------|  `   |           |  '   |------+------+------+------+------+--------|
+ * | LShift | Z/L2 |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  L1  |  '"  | LAlt | LCmd |  Esc |                                       |  Esc | RCmd | RAlt |      |  L1  |
+ *   |  L1  | '/L1 | LAlt | LCmd |  Esc |                                       |  Esc | RCmd | RAlt |      |  L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
  *                                        |      | L1   |       | L1   |Ctrl/Esc|
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | L2   |       | L2   |        |      |
- *                                 |Space |Backsp|------|       |------|  Del   |Enter |
+ *                                 |Space |Backsp|------|       |------|  Del   |Space |
  *                                 |      |ace   | Esc  |       |      |        |      |
  *                                 `--------------------'       `----------------------'
  */
@@ -40,23 +40,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_EQL,   KC_1,           KC_2,    KC_3,    KC_4,   KC_5, KC_NO,
-        KC_TAB,   KC_Q,           KC_W,    KC_E,    KC_R,   KC_T, KC_LBRC,
-        KC_LCTRL, KC_A,           KC_S,    KC_D,    KC_F,   KC_G,
-        KC_LSFT,  LT(MDIA, KC_Z), KC_X,    KC_C,   KC_V, KC_B,    KC_EQL,
-        MO(SYMB), KC_QUOT,        KC_LALT, KC_LGUI, KC_ESC,
+        KC_EQL,   KC_1,                KC_2,    KC_3,    KC_4,   KC_5, KC_NO,
+        KC_TAB,   KC_Q,                KC_W,    KC_E,    KC_R,   KC_T, KC_LBRC,
+        KC_LCTRL, KC_A,                KC_S,    KC_D,    KC_F,   KC_G,
+        KC_LSFT,  LT(MDIA, KC_Z),      KC_X,    KC_C,    KC_V,   KC_B, KC_GRV,
+        MO(SYMB), LT(SYMB, KC_QUOT),   KC_LALT, KC_LGUI, KC_ESC,
                                                       KC_NO,   MO(SYMB),
                                                                MO(MDIA),
                                               KC_SPC, KC_BSPC, KC_ESC,
         // right hand
         KC_NO,    KC_6, KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
         KC_RBRC,  KC_Y, KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-                  KC_H, KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_MINS,  KC_N, KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                  KC_H, KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+        KC_QUOT,  KC_N, KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                         KC_ESC,  KC_RGUI, KC_RALT, KC_NO,   MO(SYMB),
         MO(SYMB), KC_ESC,
         MO(MDIA),
-        KC_NO,    KC_DEL, KC_ENT
+        KC_NO,    KC_DEL, KC_SPC
     ),
 /* Keymap 1: Symbol Layer
  *
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
  * |         |   !  |   @  |   {  |   }  |   |  |      |           |      |   Up |   7  |   8  |   9  |   *  |   F12  |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |         |   #  |   $  |   (  |   )  |   `  |------|           |------| Down |   4  |   5  |   6  |   +  |        |
+ * |         |   #  |   $  |   (  |   )  |   `  |------|           |------| Left | Down |  Up  |Right |   +  |        |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |         |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |   &  |   1  |   2  |   3  |   \  |        |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -93,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        // right hand
        KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
        KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
-                KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
+                KC_LEFT, KC_DOWN,KC_UP,   KC_RGHT, KC_PLUS, KC_TRNS,
        KC_TRNS, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
                          KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
        RGB_TOG, RGB_SLD,
