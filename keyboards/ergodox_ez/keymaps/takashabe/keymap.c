d357290eae36381a1c79fa2937661c5b22a59871
takashabe <abe_takashi@cyberagent.co.jp>
2018-11-19 19:38:56 +0900

    fix none key

--- keyboards/ergodox_ez/keymaps/takashabe/keymap.c
+++ keyboards/ergodox_ez/keymaps/takashabe/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|  `   |           |  '   |------+------+------+------+------+--------|
  * | LShift | Z/L2 |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  L1  |  L1  | LAlt | LCmd |  Esc |                                       |  Esc | RCmd | RAlt |      |  L1  |
+ *   |      |  L1  | LAlt | LCmd |  Esc |                                       |  Esc | RCmd | RAlt |  L1  |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
  *                                        |      | L1   |       | L1   |Ctrl/Esc|
@@ -44,16 +44,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,           KC_W,    KC_E,    KC_R,   KC_T, KC_LBRC,
         KC_LCTRL, KC_A,           KC_S,    KC_D,    KC_F,   KC_G,
         KC_LSFT,  LT(MDIA, KC_Z), KC_X,    KC_C,    KC_V,   KC_B, KC_GRV,
-        MO(SYMB), SYMB,           KC_LALT, KC_LGUI, KC_ESC,
+        KC_NO,    MO(SYMB),       KC_LALT, KC_LGUI, KC_ESC,
                                                       KC_NO,   MO(SYMB),
                                                                MO(MDIA),
                                               KC_SPC, KC_BSPC, KC_ESC,
         // right hand
-        KC_NO,    KC_6, KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS,
-        KC_RBRC,  KC_Y, KC_U,   KC_I,    KC_O,    KC_P,    KC_BSLS,
-                  KC_H, KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_QUOT,  KC_N, KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                        KC_ESC, KC_RGUI, KC_RALT, KC_NO,   MO(SYMB),
+        KC_NO,    KC_6, KC_7,   KC_8,    KC_9,    KC_0,     KC_MINS,
+        KC_RBRC,  KC_Y, KC_U,   KC_I,    KC_O,    KC_P,     KC_BSLS,
+                  KC_H, KC_J,   KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
+        KC_QUOT,  KC_N, KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
+                        KC_ESC, KC_RGUI, KC_RALT, MO(SYMB), KC_NO,
         MO(SYMB), KC_ESC,
         MO(MDIA),
         KC_NO,    KC_DEL, KC_ENT
