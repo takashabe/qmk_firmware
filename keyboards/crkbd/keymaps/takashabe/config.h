2d75affbc419f695f22e4d4eb0f27d7f4d0f60af
takashabe <i@takashabe.dev>
2023-03-08 03:13:08 +0900

    led

--- keyboards/crkbd/keymaps/takashabe/config.h
+++ keyboards/crkbd/keymaps/takashabe/config.h
@@ -46,6 +46,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     #define RGBLIGHT_HUE_STEP 10
     #define RGBLIGHT_SAT_STEP 17
     #define RGBLIGHT_VAL_STEP 17
+    #define RGBLIGHT_DEFAULT_HUE 127 // cyan
 #endif
 
 #define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
