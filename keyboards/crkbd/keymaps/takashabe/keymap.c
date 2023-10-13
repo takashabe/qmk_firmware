41a14bdf2690a5e40cffb8be1e777034c1400cbb
takashabe <i@takashabe.dev>
2023-10-13 10:39:35 +0900

    feat: home/end

--- keyboards/crkbd/keymaps/takashabe/keymap.c
+++ keyboards/crkbd/keymaps/takashabe/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, XXXXXXX, XXXXXXX, KC_MINS, KC_UNDS, XXXXXXX,                      KC_BSPC, KC_ESC,   KC_EQL, KC_PLUS,  KC_GRV, KC_TILD,
+      KC_LCTL, KC_HOME,  KC_END, KC_MINS, KC_UNDS, XXXXXXX,                      KC_BSPC, KC_ESC,   KC_EQL, KC_PLUS,  KC_GRV, KC_TILD,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_UNDS, KC_MINS, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
